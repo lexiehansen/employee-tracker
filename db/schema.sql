dadc0a27a801b902b2cf45dcdafaee8efb93a71a
Lexie Hansen <home@Lexies-MacBook-Pro.local>
2021-04-22 13:11:10 -0600

    schema tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+-- setup --
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+-- dept table --
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+);
+
+
+-- roles table --
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+);
+
+-- employee table --
+CREATE TABLE employee(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INT,
+    manager_id INT,
+);
