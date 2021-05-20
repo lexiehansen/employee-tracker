1b8c8f8c71e0cac9356e45cbaea9b8b8e49e0415
Lexie Hansen <home@Lexies-MacBook-Pro.local>
2021-05-19 23:04:02 -0600

    add role and employee functions

--- index.js
+++ index.js
@@ -100,3 +100,129 @@ addDepartment = () => {
       )
     })
 };
+
+addRole = () => {
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+
+    const arr = res.map(({ id, name }) => ({
+      name: name,
+      value: id,
+    }));
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "Role title:",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "Salary:",
+        },
+        {
+          type: "list",
+          name: "dept",
+          message: "Department:",
+          choices: arr,
+        },
+      ])
+      .then((answer) => {
+        console.log(answer);
+        connection.query(
+          "INSERT INTO roles SET ?",
+          [
+            {
+              title: answer.title,
+              salary: answer.salary,
+              department_id: answer.dept,
+            },
+          ],
+          function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            questions();
+          }
+        )
+      })
+  })
+};
+
+addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first",
+        message: "Employee First Name:",
+      },
+      {
+        type: "input",
+        name: "last",
+        message: "Last Name:",
+      },
+    ])
+    .then((answer) => {
+      let firstName = answer.first;
+      let lastName = answer.last;
+
+      connection.query(`SELECT * FROM roles`, function (err, res) {
+        if (err) throw err;
+        const roleChoices = res.map(({ id, title }) => ({
+          name: title,
+          value: id,
+        }));
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "role",
+              message: "Role:",
+              choices: roleChoices,
+            },
+          ])
+          .then((answer) => {
+            let roleId = answer.roles;
+
+            connection.query(`SELECT * FROM employee`, function (err, res) {
+              if (err) throw err;
+              const managerChoices = res.map(
+                ({ id, first_name, last_name, man_ind }) => ({
+                  name: first_name + " " + last_name,
+                  value: id,
+                  man_ind,
+                })
+              );
+              inquirer
+                .prompt([
+                  {
+                    type: "list",
+                    name: "manager",
+                    message: "Manager:",
+                    choices: managerChoices,
+                  },
+                ])
+
+                .then((answer) => {
+                    connection.query(
+                      "INSERT INTO employee SET ?",
+                      {
+                        first_name: firstName,
+                        last_name: lastName,
+                        roles_id: roleId,
+                        manager_id: answer.manager,
+                      },
+
+                      function (err, res) {
+                        if (err) throw err;
+                        console.table(res);
+                        questions();
+                      }
+                    )
+                })
+            })
+          })
+      })
+    })
+};
